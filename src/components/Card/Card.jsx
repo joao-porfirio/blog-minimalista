bb78492c147e5e16de6b11e767f077ad14526ff6
Joao Porfirio <joaoprf54@gmail.com>
2021-09-23 12:07:44 -0300

    Hook para alterar o ícone do card nos eventos in e out

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,28 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import HeartLogo from '../../assets/icons/heart.svg';
+import Heart from '../../assets/icons/heart.svg';
+import FullHeart from '../../assets/icons/full.svg';
 import '../../reset/reset.css';
 import '../../css/style.css';
 
-
 const Card = (props) => {
+  const [heart, setHeart] = useState(Heart);
   useEffect(() => {
     Aos.init({
       duration:1000,
       delay: 500
     });
   }, [])
+
   return (
     <section className="card-container">
       <div data-aos="fade-right" className="card-container card">
         <div className="card__info-top">
           <span className="card__data">{props.data}</span>
-          <img 
-          onMouseOver={e =>(e.currentTarget.src="/static/media/heart_full.e0e2f892.svg")}
-          onMouseOut={e =>(e.currentTarget.src="/static/media/heart.5efc8d5a.svg")}
+          <img onMouseEnter={()=>{setHeart(FullHeart)}} 
+            onMouseOut={()=>{setHeart(Heart)}}
+          alt=""
           className="card__heart-logo" 
-          src={HeartLogo}/>
+          src={heart}/>
         </div>
         <h1 className="card__titulo">{props.titulo}</h1>
         <p className="card__texto">{props.texto}</p>
